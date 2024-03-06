76c75abdcdcaa6762bca3f6180df7cfbaa95a7f3
Patrick Alvarez Eades <p.alvarezeades@gmail.com>
2024-03-06 16:41:46 +0100

    Metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,41 @@ const karla = Karla({
 export const metadata = {
     title: 'Vote to Play!',
     description: 'Helping you avoid arguments by voting on the next game',
+    creator: 'Patrick Alvarez',
+    openGraph: {
+        title: 'Vote to Play!',
+        description: 'Helping you avoid arguments by voting on the next game',
+        url: 'https://vote-game-five.vercel.app/',
+        siteName: 'Vote to Play!',
+        images: [
+            {
+                url: 'https://vote-game-five.vercel.app/images/social2.png', // Must be an absolute URL
+                width: 800,
+                height: 600,
+            },
+            {
+                url: 'https://vote-game-five.vercel.app/images/social2.png', // Must be an absolute URL
+                width: 1800,
+                height: 1600,
+                alt: 'My custom alt',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+    robots: {
+        index: false,
+        follow: true,
+        nocache: true,
+        googleBot: {
+            index: true,
+            follow: false,
+            noimageindex: true,
+            'max-video-preview': -1,
+            'max-image-preview': 'large',
+            'max-snippet': -1,
+        },
+    },
 };
 
 export default function RootLayout({ children }) {
